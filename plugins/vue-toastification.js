e44e18d1db61ff51f322492ebf2fed1d65c6735e
sheymantech <olaniyansheyi1704@gmail.com>
2024-07-02 03:33:07 +0100

    new

--- plugins/vue-toastification.js
+++ plugins/vue-toastification.js
@@ -1,12 +1,26 @@
+// import { defineNuxtPlugin } from "#app";
+// import * as vt from "vue-toastification";
+// import "vue-toastification/dist/index.css";
+
+// export default defineNuxtPlugin((nuxtApp) => {
+//   nuxtApp.vueApp.use(vt.default);
+//   return {
+//     provide: {
+//       toast: vt.useToast(),
+//     },
+//   };
+// });
+
 import { defineNuxtPlugin } from "#app";
-import * as vt from "vue-toastification";
+import Toast, { useToast } from "vue-toastification";
 import "vue-toastification/dist/index.css";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(vt.default);
+  nuxtApp.vueApp.use(Toast);
+  const toast = useToast();
   return {
     provide: {
-      toast: vt.useToast(),
+      toast: toast,
     },
   };
 });
